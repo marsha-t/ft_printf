698d43e44d1463eb466e499e17be1c867afe98e9
Marsha <pgmarshateo@gmail.com>
2024-01-05 11:18:45 +0400

    update mandatory portion after testing in Linux

---
+++ ft_printf.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mateo <mateo@student.42abudhabi.ae>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/04 18:25:11 by mateo             #+#    #+#             */
+/*   Updated: 2024/01/04 18:25:11 by mateo            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_charinstr(char c, char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_printconv(const char *str, va_list va_ptr)
+{
+	int	count; 
+	
+	count = 0;
+	if (*str == '%')
+		count = ft_conv_c('%');
+	else if (*str == 'c')
+		count = ft_conv_c(va_arg(va_ptr, int));
+	else if (*str == 's')
+		count = ft_conv_s(va_arg(va_ptr, char *));
+	else if (*str == 'p')
+		count = ft_conv_p(va_arg(va_ptr, unsigned long long));
+	else if (*str == 'd' || *str == 'i')
+		count = ft_conv_d(va_arg(va_ptr, int));
+	else if (*str == 'u')
+		count = ft_conv_u(va_arg(va_ptr, unsigned int));
+	else if (*str == 'x')
+		count = ft_conv_x(va_arg(va_ptr, unsigned int), BASE16_SMALL);
+	else if (*str == 'X')
+		count = ft_conv_x(va_arg(va_ptr, unsigned int), BASE16_LARGE);
+	return (count);
+}
+
+int	ft_printf(const char *str, ...)
+{
+	int		count;
+	va_list	va_ptr;
+
+	count = 0;
+	va_start(va_ptr, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			if (ft_charinstr(*str, CONVERSIONS))
+			{
+				count += ft_printconv(str, va_ptr);
+				str++;
+			}
+			else
+				return (-1);
+		}
+		else
+		{
+			count += ft_conv_c(*str);
+			str++;
+		}
+	}
+	va_end(va_ptr);
+	return (count);
+}
